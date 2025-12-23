214bfa86f7371b073604106c0aa5d6e6fb4122d0
keyushhh <keyushh@gmail.com>
2025-12-23 17:17:54 +0530

    Navigation UI Fix

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -133,10 +133,10 @@ const Homepage = () => {
   {/* Floating + Button (FAB) */}
 <button
   className="absolute left-1/2 -translate-x-1/2 z-10
-             w-[86px] h-[86px] rounded-full
+             w-[84px] h-[84px] rounded-full
              flex items-center justify-center"
   style={{
-    bottom: "-2px", // pushed ~10px lower than before
+    bottom: "-4px", // pushed ~10px lower than before
   }}
 >
   <img
